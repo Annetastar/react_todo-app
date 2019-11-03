bb09d0646d72a7c29b1e90b07c28f2facde38601
Anna Kazantseva <annetastara@gmail.com>
2019-11-03 19:29:28 +0200

    Updates

---
+++ precache-manifest.90516b4dd82d66b41a1d984c10b0c213.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7ae7346b2855332dfeae3632bfe4e5",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "9dcfc67ce5c5c0d39b26",
+    "url": "/react_todo-app/static/css/main.2215c207.chunk.css"
+  },
+  {
+    "revision": "a99f212f10d8fa541fe6",
+    "url": "/react_todo-app/static/js/2.63e32509.chunk.js"
+  },
+  {
+    "revision": "9dcfc67ce5c5c0d39b26",
+    "url": "/react_todo-app/static/js/main.e39d3789.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
